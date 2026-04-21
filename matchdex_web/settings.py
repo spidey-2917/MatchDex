94358aae2d073e3b6485a245c18cfb75c22615b5
User <user@example.com>
2026-04-22 03:23:14 +0530

    Fix static files styling with WhiteNoise

--- matchdex_web/settings.py
+++ matchdex_web/settings.py
@@ -51,6 +51,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
